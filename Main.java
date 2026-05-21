125fd13b1fa1f7fcaf8544a8df2c605b9581b124
Harish Reddy <harishreddy.g23@gmail.com>
2026-05-21 18:20:29 +0530

    Remove unused  comments
    Removed commented-out Calc class and related code.

--- Main.java
+++ Main.java
@@ -3,27 +3,6 @@ import java.net.HttpURLConnection;
 import java.io.InputStream;
 
 
-// class Calc {
-
-//     int i ;
-//     int j;
-//     Calc(int i , int j){
-//         this.i = i;
-//         this.j =j;
-//     }
-//     public int add(){
-//         return i+j;
-//     }
-//     public int sub(){
-//         return i-j;
-//     }
-//     public int multiply(){
-//         return i*j;
-//     }
-// }
-
-
-
 
 public class Main {
     public static void main(String[] args) {
@@ -48,20 +27,6 @@ public class Main {
         }catch(Exception e){
             System.out.println(e);
         }
-
-
-
-
-
-        // System.out.println("===  Student Info: ===");
-        // System.out.println("Harish Reddy");
-        // System.out.println("Age: 20");
-        // System.out.println("Roll No : 85");
-        // Calc c = new Calc(2,2);
-        
-        // System.out.println(c.add());
-        // System.out.println(c.sub());
-        // System.out.println(c.multiply());
 
     }
 }
