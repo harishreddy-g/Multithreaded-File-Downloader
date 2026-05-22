7e8858c5e4f4ec928a3c6e1e1dee055e7559a95e
harishreddy-g <harishreddy.g23@gmail.com>
2026-05-22 16:32:45 +0530

    Used FileOutputstream class and downloaded webpage hmtl of google and some random image

--- Main.java
+++ Main.java
@@ -1,67 +1,36 @@
 import java.net.URL;
 import java.net.HttpURLConnection;
 import java.io.InputStream;
-
-
-// class Calc {
-
-//     int i ;
-//     int j;
-//     Calc(int i , int j){
-//         this.i = i;
-//         this.j =j;
-//     }
-//     public int add(){
-//         return i+j;
-//     }
-//     public int sub(){
-//         return i-j;
-//     }
-//     public int multiply(){
-//         return i*j;
-//     }
-// }
-
-
-
+import java.io.FileOutputStream;
 
 public class Main {
     public static void main(String[] args) {
-        try{
-           
-            URL url = new URL("https://google.com");
+        try {
+
+            URL url = new URL("https://picsum.photos/200");
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
-             InputStream in = con.getInputStream();
-            int responseCode = con.getResponseCode();
-            int length= con.getContentLength();
-            System.out.println("Response Code : "+ responseCode);
-            System.out.println(length);
-            int data;
+            InputStream in = con.getInputStream();
+            FileOutputStream out= new FileOutputStream("img.jpg");
 
-            while((data= in.read())<1480){
-                System.out.print((char)data);
+            // int responseCode = con.getResponseCode();
+            // int length = con.getContentLength();
+            // System.out.println("Response Code : " + responseCode);
+            // System.out.println(length);
+            byte[] buffer = new byte[4096];
+            int byteread;
 
+            while ((byteread = in.read(buffer)) !=-1) {
+                System.out.println(byteread);
+                out.write(buffer, 0 , byteread);
             }
-           
+
 
             in.close();
-        }catch(Exception e){
+            out.close();
+            System.out.println("download completed");
+        } catch (Exception e) {
             System.out.println(e);
         }
-
-
-
-
-
-        // System.out.println("===  Student Info: ===");
-        // System.out.println("Harish Reddy");
-        // System.out.println("Age: 20");
-        // System.out.println("Roll No : 85");
-        // Calc c = new Calc(2,2);
-        
-        // System.out.println(c.add());
-        // System.out.println(c.sub());
-        // System.out.println(c.multiply());
 
     }
 }
