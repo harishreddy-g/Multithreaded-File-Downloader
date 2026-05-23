451cbedf17a2db49a4df7a0d834d86deaa053843
harishreddy-g <harishreddy.g23@gmail.com>
2026-05-23 15:31:42 +0530

    Organised code upto now

--- Main.java
+++ Main.java
@@ -4,33 +4,50 @@ import java.io.InputStream;
 import java.io.FileOutputStream;
 
 public class Main {
+
     public static void main(String[] args) {
+
         try {
 
             URL url = new URL("https://picsum.photos/200");
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+            HttpURLConnection con =
+                    (HttpURLConnection) url.openConnection();
+
+            int responseCode = con.getResponseCode();
+
+            int fileSize = con.getContentLength();
+
+            System.out.println("Response Code: " + responseCode);
+
+            System.out.println("File Size: " + fileSize);
+
             InputStream in = con.getInputStream();
-            FileOutputStream out= new FileOutputStream("img.jpg");
 
-            // int responseCode = con.getResponseCode();
-            // int length = con.getContentLength();
-            // System.out.println("Response Code : " + responseCode);
-            // System.out.println(length);
+            FileOutputStream out =
+                    new FileOutputStream("img.jpg");
+
             byte[] buffer = new byte[4096];
-            int byteread;
 
-            while ((byteread = in.read(buffer)) !=-1) {
-                System.out.println(byteread);
-                out.write(buffer, 0 , byteread);
+            int bytesRead;
+
+            while((bytesRead = in.read(buffer)) != -1) {
+
+                System.out.println(bytesRead);
+
+                out.write(buffer, 0, bytesRead);
+
             }
-
 
             in.close();
             out.close();
-            System.out.println("download completed");
-        } catch (Exception e) {
-            System.out.println(e);
-        }
 
+            System.out.println("Download Complete");
+
+        } catch(Exception e) {
+
+            System.out.println(e);
+
+        }
     }
 }
